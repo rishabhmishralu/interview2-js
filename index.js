42d32d81cf0a5dbbc4b36801712e4cffc46264c6
= <=>
2022-12-29 18:58:33 +0530

    add

---
+++ index.js
@@ -0,0 +1,20 @@
+// function student(name,rollno,city,age){
+// this.name=name;
+// this.rollno=rollno;
+// this.city=city;
+// this.age=age;
+
+// }
+//  const students= new student("rishabh",20,"lucknow",22)
+//  console.log(students)
+
+
+// callback
+// function hi(){
+//     console.log("hello");
+// }
+// function name(a){
+//     console.log("rishbah");
+//     hi()
+// }
+// name()
