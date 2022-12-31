98cfe258426a94e53795a1365f828b3d97308fc9
= <=>
2022-12-31 14:25:43 +0530

    changes

--- index.js
+++ index.js
@@ -18,3 +18,28 @@
 //     hi()
 // }
 // name()
+
+
+// prototype
+
+// function student(name){
+//     this.name=name;
+
+// }
+
+// student.prototype.city="lmp"
+// students=new student("rahul")
+// console.log(students.city);
+
+
+function student(name,rollNo){
+    this.name=name;
+    this.rollNo=rollNo;
+}
+let student1= new student("rishabh",08)
+let student2= new student("aryan",02)
+console.log(student1);
+console.log(student2);
+student1.sports='cricket'
+
+console.log(Object.getOwnPropertyNames(student1))
